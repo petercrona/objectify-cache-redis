f9877da11e83694afc73253de6db7c0d0fa331dc
Peter Crona <petercrona89@gmail.com>
2023-09-01 16:11:41 +0200

    add missing import

--- src/main/java/com/googlecode/objectify/cache/redis/RedisMemcacheService.java
+++ src/main/java/com/googlecode/objectify/cache/redis/RedisMemcacheService.java
@@ -18,6 +18,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
